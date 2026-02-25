368cae4c84f98d36cbef7af693f71cbc21727a27
shiv <shivanshmoore05@gmail.com>
2026-02-25 08:33:54 +0000

    tab view

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,28 @@
-import { StyleSheet, View, Text } from 'react-native';
-import PagerView from 'react-native-pager-view';
+import { StyleSheet, View } from "react-native";
+import PagerView from "react-native-pager-view";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Social from "../components/Social";
+import Personal from "../components/Personal";
+import TabBar from "../components/TabBar";
 
 export default function MyPager() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <TabBar />
       <PagerView style={styles.container} initialPage={0}>
-        <View style={styles.page} key="1">
-          <Text>First page</Text>
-          <Text>Swipe ➡️</Text>
+        <View key="0">
+          <Personal />
         </View>
-        <View style={styles.page} key="2">
-          <Text>Second page</Text>
-        </View>
-        <View style={styles.page} key="3">
-          <Text>Third page</Text>
+        <View key="1">
+          <Social />
         </View>
       </PagerView>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  page: {
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
---
+++ components/TabBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function TabBar() {
+  return (
+    <View>
+      <Text>Tabbar</Text>
+    </View>
+  );
+}
