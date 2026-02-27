5c61973c6b07c16a336aefe07ad3cb9424483dad
shiv <shivanshmoore05@gmail.com>
2026-02-27 12:39:13 +0000

    added perosnal files

---
+++ components/GoalTile.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function GoalTile() {
+  return (
+    <View>
+      <Text>GoalTile</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ components/ScreenTimeBanner.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Colours } from "../constants/Colours";
+import { Fonts } from "../constants/Fonts";
+
+export default function ScreenTimeBanner() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.dot} />
+      <Text style={styles.text}>All apps locked · 0m today</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+    paddingVertical: 12,
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#34C759",
+  },
+  text: {
+    color: Colours.secondaryText,
+    fontFamily: Fonts.medium,
+    fontSize: 13,
+  },
+});
