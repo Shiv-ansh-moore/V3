e2b5e9a4b1e62e518cd11e93fa57e38910b7af3e
Shivansh <shivanshmoore05@gmail.com>
2026-03-24 22:33:52 +0000

    Set 4 done

--- modules/screen-time-locks/index.ts
+++ modules/screen-time-locks/index.ts
@@ -1,5 +1,7 @@
-// Reexport the native module. On web, it will be resolved to ScreenTimeLocksModule.web.ts
-// and on native platforms to ScreenTimeLocksModule.ts
-export { default } from './src/ScreenTimeLocksModule';
-export { default as ScreenTimeLocksView } from './src/ScreenTimeLocksView';
-export * from  './src/ScreenTimeLocks.types';
+import { requireNativeModule } from 'expo';
+
+const ScreenTimeLocks = requireNativeModule('ScreenTimeLocks')
+
+export async function requestAuthorization(): Promise<string> {
+  return await ScreenTimeLocks.requestAuthorization();
+}
