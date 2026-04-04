31307e646569779cd8749e35464140c3b8963a1b
shiv <shivanshmoore05@gmail.com>
2026-04-04 11:50:09 +0100

    Started android

--- modules/screen-time-locks/android/src/main/java/expo/modules/screentimelocks/ScreenTimeLocksModule.kt
+++ modules/screen-time-locks/android/src/main/java/expo/modules/screentimelocks/ScreenTimeLocksModule.kt
@@ -2,49 +2,42 @@ package expo.modules.screentimelocks
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import java.net.URL
 
 class ScreenTimeLocksModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ScreenTimeLocks')` in JavaScript.
     Name("ScreenTimeLocks")
 
-    // Defines constant property on the module.
-    Constant("PI") {
-      Math.PI
+    AsyncFunction("requestAuthorization") {
+      return@AsyncFunction "not_supported"
     }
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
+    AsyncFunction("showAppPicker") {
+      return@AsyncFunction mapOf("selectedApps" to 0)
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
+    AsyncFunction("blockApps") {
+      return@AsyncFunction mapOf("blocked" to 0)
     }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ScreenTimeLocksView::class) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { view: ScreenTimeLocksView, url: URL ->
-        view.webView.loadUrl(url.toString())
-      }
-      // Defines an event that the view can send to JavaScript.
-      Events("onLoad")
+    AsyncFunction("unblockApps") {
+      return@AsyncFunction "not_supported"
+    }
+
+    AsyncFunction("manageBlockedApps") {
+      return@AsyncFunction mapOf("cancelled" to true)
+    }
+
+    AsyncFunction("unlockForDuration") { minutes: Int, reason: String ->
+      return@AsyncFunction "not_supported"
+    }
+
+    AsyncFunction("relockNow") {
+      return@AsyncFunction "not_supported"
+    }
+
+    Function("getActiveUnlock") {
+      return@Function null
     }
   }
 }
+
