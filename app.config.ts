cedc8d94f0e6e4401d0c22e087e103dcf8bc52b5
shiv <shivanshmoore05@gmail.com>
2026-05-18 01:32:09 +0100

    notifaction set up

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: config.name ?? "V3App",
   slug: config.slug ?? "V3App",
   extra: {
+    ...(config.extra ?? {}),
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
     supabasePublishableKey: process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
   },
